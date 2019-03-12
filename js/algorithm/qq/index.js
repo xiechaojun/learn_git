c72a6b4884dd4da3ce31284e3ae99d51a9237f4f
ciechaojun <184613486@qq.com>
2019-03-12 22:26:52 +0800

    QQ

---
+++ js/algorithm/qq/index.js
@@ -0,0 +1,24 @@
+// 数组是最廉价的数据结构
+let enc_qq = [6,3,1,7,5,8,9,2,4];
+ qq = [];//真正的
+head  = 0,//队首指针，一直指向要移除的元素位置
+tail = 9;//队尾指针，要加入的元素位置
+//  第一个数移除
+while (head < tail)
+{
+qq.push(enc_qq[head]);
+head++;
+enc_qq[tail] = enc_qq[head];  //也可用push？
+tail++;
+head++;
+}
+// enc_qq.shift();
+console.log(typeof qq);
+// enc_qq.shift();//shift移除元素
+// console.log(enc_qq);
+// enc_qq.unshift(0);   //添加
+// console.log(enc_qq);
+// enc_qq.pop();
+// console.log(enc_qq)
+//字符串是字符集合
+// console.log(enc_qq.length,enc_qq[0]);
